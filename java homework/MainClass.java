50806df44bb62d9b95bd237a7c1243f175672313
Michał Słupczyński <Asmodiel@Antares.local>
2013-11-29 22:00:05 +0100

    refactored __every__thing ^^

--- java homework/MainClass.java
+++ java homework/MainClass.java
@@ -15,14 +15,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 public class MainClass extends JApplet implements ActionListener
- {
-        //test
+{
         JPanel mainPanel;
         JPanel titlePanel;
         JPanel contentPanel;
@@ -48,157 +46,154 @@ public class MainClass extends JApplet implements ActionListener
         JRadioButton correctRadio;
         JRadioButton wrongRadio;
 
-public void createTitle(String title)
-        {
-                titlePanel = new JPanel();
-                titlePanel.setName("title");
-                titlePanel.setBackground( Color.orange );
-                titlePanel.setPreferredSize( new Dimension( 500, 50 ) );
-                
-                titlePanel.add( new JLabel(title), BorderLayout.PAGE_START );
-                
-                 mainPanel.add( titlePanel );
-        }
-        
-        public void createQuestionPanel(int in, String question, int id, String [] answers)
-        {
-                JPanel  tempPanel = new JPanel();
-                                tempPanel.setName( "" + id );
-                                tempPanel.setBackground( Color.green );
-                                tempPanel.setAlignmentX( JPanel.CENTER_ALIGNMENT );
-                                        
-                JLabel  tempLabel = new JLabel( createImageIcon("/images/img1.jpg") );
-                                tempLabel.setAlignmentX( JPanel.CENTER_ALIGNMENT );
-                                tempPanel.add( tempLabel );
-                                
-                                tempPanel.add( new JLabel(question) );
-                                
-                for( int i = 0; i < answers.length; i++ )
-                {
-                        JRadioButton temp = new JRadioButton( answers[ i ] );
-                        allgroups[ in ].add( temp );
-                        tempPanel.add( temp );
-                        if( temp_correct[ id ] == i )
-                        {
-                                correct[ id ] = temp.getModel( );
-                        }
-                }
-                
-                                panelList.add( tempPanel );
-        }
-	
-	public JButton createButton(String name)
-	{
-		
-		JButton button = new JButton(name);
-		
-		button.addActionListener(this);
-		
-		bottomPanel.add( button );
-		
-		return button;
-	}
-	
-	
-	public void printPanels()
-	{
-		Iterator < JPanel >    panelIT =  panelList.iterator();			
-        while ( panelIT.hasNext() )
-        {
-        	JPanel  currPanel = panelIT.next();
-	        		currPanel.setVisible(false);
-	        		contentPanel.add ( currPanel );
-        }
-		
-		this.add( contentPanel );
-				
-	}
-	
-	protected static ImageIcon createImageIcon(String path)
-	{
-	    URL imgURL = MainClass.class.getResource(path);
-		
-		return new ImageIcon( imgURL );
-	}	
-	
-	public void init()
-	{
-		score = 0;
-		this.setSize( 500, 600 );
-		
-		// create main panel
-		mainPanel = new JPanel( );
-		mainPanel.setSize( 500, 500 );
-		//mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));	// BOX LAYOUT
-		mainPanel.setBackground( Color.gray );		
-		
-		contentPanel = new JPanel();
-			contentPanel.setBackground( Color.WHITE );
-			contentPanel.setLayout( new BoxLayout( contentPanel, BoxLayout.Y_AXIS ) );
-			contentPanel.setPreferredSize( new Dimension( 500, 500 ) );
-		
-		bottomPanel = new JPanel();
-		
-			bottomPanel.setPreferredSize( new Dimension( 500, 50 ) );
-			bottomPanel.setBackground( Color.lightGray );
+		public void createTitle(String title)
+		{
+	        titlePanel = new JPanel();
+	        titlePanel.setName("title");
+	        titlePanel.setBackground( Color.orange );
+	        titlePanel.setPreferredSize( new Dimension( 500, 50 ) );
+	        
+	        titlePanel.add( new JLabel(title), BorderLayout.PAGE_START );
+	        
+	         mainPanel.add( titlePanel );
+		}
+		        
+		public void createQuestionPanel(int in, String question, int id, String [] answers)
+		{
+	        JPanel  tempPanel = new JPanel();
+	                tempPanel.setName( "" + id );
+	                tempPanel.setBackground( Color.green );
+	                tempPanel.setAlignmentX( JPanel.CENTER_ALIGNMENT );
+	                                
+	        JLabel  tempLabel = new JLabel( createImageIcon("/images/img1.jpg") );
+	                tempLabel.setAlignmentX( JPanel.CENTER_ALIGNMENT );
+	                tempPanel.add( tempLabel );
+	                
+	                tempPanel.add( new JLabel(question) );
+	                        
+	        for( int i = 0; i < answers.length; i++ )
+	        {
+	                JRadioButton temp = new JRadioButton( answers[ i ] );
+	                allgroups[ in ].add( temp );
+	                tempPanel.add( temp );
+	                if( temp_correct[ id ] == i )
+	                {
+	                        correct[ id ] = temp.getModel( );
+	                }
+	        }
+	        
+	        panelList.add( tempPanel );
+		}
 			
-			statusLabel = new JLabel(" status ");
-			bottomPanel.add( statusLabel );
+		public JButton createButton(String name)
+		{
 			
-		
-			//MAIN FOR HOMEWERK
-			createTitle("Sample C Driver Written Test");
-			String[] questions = new String [PANELS];
-			questions[0]="1. You may drive off of the paved roadway to pass another vehicle:";
-			questions[1]="2. What is the meaning of this sign?";
-			questions[2]="3. What is the meaning of this sign?";
+			JButton button = new JButton(name);
+					button.addActionListener( this );
 			
-			String[][] answers = new String [PANELS][3];
-			answers[0][0] = "if the vehicle ahead of you is turning left";
-			answers[0][1] = "if the vehicle ahead of you is turning right";
-			answers[0][2] = "under no circumstances";
-
-			answers[1][0] = "give way";
-			answers[1][1] = "no entry for vehicular traffic";
-			answers[1][2] = "give way to traffic on your right";
-
-			answers[2][0] = "no motor vehicles";
-			answers[2][1] = "no pedestrians";
-			answers[2][2] = "no parking";			
-
-			for(int i=0;i<PANELS;i++)
-			{
-				temp_correct[i] = i%3;
-				allgroups[i] = new ButtonGroup();
-				createQuestionPanel(i,questions[i], i, answers[i]);
-			}		
+			bottomPanel.add( button );
 			
-			prevButton = createButton("Previous");
-			prevButton.setVisible(false);
-			nextButton = createButton("Next");
-			submitButton = createButton("Submit");
-			quitButton = createButton("Quit");
-			printPanels();
-			
-			panelList.get(0).setVisible(true);
-			
-
-		
-			mainPanel.add( contentPanel, BorderLayout.CENTER );
-			mainPanel.add( bottomPanel, BorderLayout.PAGE_END );
-		
-		this.add( mainPanel );
-		
+			return button;
 		}
 		
-
+			
+		public void printPanels()
+		{
+			Iterator < JPanel > panelIT = panelList.iterator();
+			
+		    while ( panelIT.hasNext() )
+		    {
+		    	JPanel  currPanel = panelIT.next();
+		        		currPanel.setVisible( false );
+		        		contentPanel.add ( currPanel );
+		    }
+			
+			this.add( contentPanel );
+					
+		}
+			
+		protected static ImageIcon createImageIcon( String path )
+		{
+		    URL imgURL = MainClass.class.getResource(path);
+			
+			return new ImageIcon( imgURL );
+		}	
+		
+		public void init()
+		{
+			score = 0;
+			
+			
+			/* set up interface */
+			this.setSize( 500, 600 );
+			mainPanel = new JPanel( );
+				mainPanel.setSize( 500, 500 );
+				mainPanel.setBackground( Color.gray );		
+			
+			contentPanel = new JPanel();
+				contentPanel.setBackground( Color.WHITE );
+				contentPanel.setLayout( new BoxLayout( contentPanel, BoxLayout.Y_AXIS ) );
+				contentPanel.setPreferredSize( new Dimension( 500, 500 ) );
+			
+			bottomPanel = new JPanel();	
+				bottomPanel.setPreferredSize( new Dimension( 500, 50 ) );
+				bottomPanel.setBackground( Color.lightGray );
+				
+			statusLabel = new JLabel(" status ");
+				bottomPanel.add( statusLabel );
+				
+			/* set up questions */
+				createTitle( "Sample C Driver Written Test" );
+				String[] questions = new String [PANELS];
+				questions[0]="1. You may drive off of the paved roadway to pass another vehicle:";
+				questions[1]="2. What is the meaning of this sign?";
+				questions[2]="3. What is the meaning of this sign?";
+				
+				String[][] answers = new String [PANELS][3];
+				answers[0][0] = "if the vehicle ahead of you is turning left";
+				answers[0][1] = "if the vehicle ahead of you is turning right";
+				answers[0][2] = "under no circumstances";
+			
+				answers[1][0] = "give way";
+				answers[1][1] = "no entry for vehicular traffic";
+				answers[1][2] = "give way to traffic on your right";
+			
+				answers[2][0] = "no motor vehicles";
+				answers[2][1] = "no pedestrians";
+				answers[2][2] = "no parking";			
+			
+				for ( int i = 0; i < PANELS; i++)
+				{
+					temp_correct[ i ] = i%3;
+					allgroups[ i ] = new ButtonGroup();
+					createQuestionPanel( i, questions[i], i, answers[i]);
+				}		
+			
+			/* set up buttons */
+				prevButton = createButton("Previous");
+				prevButton.setVisible(false);
+				nextButton = createButton("Next");
+				submitButton = createButton("Submit");
+				quitButton = createButton("Quit");
+			
+			/* set up panels */
+				printPanels();
+				
+				panelList.get(0).setVisible(true);
+				mainPanel.add( contentPanel, BorderLayout.CENTER );
+				mainPanel.add( bottomPanel, BorderLayout.PAGE_END );
+			
+				this.add( mainPanel );
+		}
+				
+		
 		@Override
 		public void actionPerformed(ActionEvent event)
 		{
 			if( event.getSource() == submitButton )
-            {     
+		    {     
 				prevButton.setVisible(false);
-        		nextButton.setVisible(false);
+				nextButton.setVisible(false);
 				score = 0;
 				
 				for ( int i = 0; i < PANELS; i++)
@@ -223,42 +218,41 @@ public void createTitle(String title)
 			        		currPanel.setVisible(false);
 		        }
 				
-            }
-            
-            if( event.getSource() == nextButton )
-            {
-            	if ( currentPanelID == 2 )
-            	{
-            		prevButton.setVisible(false);
-            		nextButton.setVisible(false);
-            		return;
-            	}
-            		prevButton.setVisible(true);
-                    panelList.get(currentPanelID).setVisible(false);
-                    panelList.get(++currentPanelID).setVisible(true);
-            }
-            
-            if ( event.getSource() == quitButton )
-            {
-            	close();
-            }
-                    
-            if( event.getSource() == prevButton )
-            {
-                    panelList.get(currentPanelID % 3).setVisible(false);
-                    panelList.get(--currentPanelID % 3).setVisible(true);
-                    if ( currentPanelID == 0 )
-                    {
-                    	prevButton.setVisible(false);
-                    	return;
-                    }
-            }
+		    }
+		    
+		    if( event.getSource() == nextButton )
+		    {
+		    	if ( currentPanelID == 2 )
+		    	{
+		    		prevButton.setVisible(false);
+		    		nextButton.setVisible(false);
+		    		return;
+		    	}
+		    		prevButton.setVisible(true);
+		            panelList.get(currentPanelID).setVisible(false);
+		            panelList.get(++currentPanelID).setVisible(true);
+		    }
+		    
+		    if ( event.getSource() == quitButton )
+		    {
+		    	close();
+		    }
+		            
+		    if( event.getSource() == prevButton )
+		    {
+		            panelList.get(currentPanelID % 3).setVisible(false);
+		            panelList.get(--currentPanelID % 3).setVisible(true);
+		            if ( currentPanelID == 0 )
+		            {
+		            	prevButton.setVisible(false);
+		            	return;
+		            }
+		    }
 		}
-			
-	void close () 
-	{
-		setVisible (false);	         
-		System.exit(0);
-	}
-
+					
+		void close () 
+		{
+			setVisible (false);	         
+			System.exit(0);
+		}
 }
