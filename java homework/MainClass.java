8ef64593cf7136322bfb4545fc509f0872f31458
Michał Słupczyński <Asmodiel@Antares.local>
2013-11-29 15:40:21 +0100

    code clean up
    bla

--- java homework/MainClass.java
+++ java homework/MainClass.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JApplet;
@@ -14,7 +17,8 @@ public class MainClass extends JApplet implements ActionListener
  {
 	JPanel mainPanel;
 	JPanel changePanel;
-	
+	JPanel tempPanel;
+	List<JPanel>  panelList  = new ArrayList  <JPanel>  ();
 	
 	JButton mybutton;
 	JLabel mylabel;	
@@ -26,45 +30,81 @@ public class MainClass extends JApplet implements ActionListener
 	
 	public void init()
 	{
-		mainPanel = new JPanel();
-		changePanel = new JPanel();
 		
-		mybutton = new JButton("OK");
-		mybutton.addActionListener(this);
+		this.setSize( 500, 500 );
 		
-		mylabel = new JLabel();
-		changeLabel = new JLabel("MOTHERFUCKING LABEL");
+
+		// create main panel
+			mainPanel = new JPanel();
+			mainPanel.setBackground( Color.gray );
+		// add a button
+			mybutton = new JButton("OK");
+			mybutton.addActionListener(this);
+			mainPanel.add(mybutton);
+		
+		// add a label
+			mylabel = new JLabel("some label");
+			mainPanel.add(mylabel);
+			//mainPanel.addHorizontalLine();
+		
 			
-		mainPanel.add(mybutton);
-		mainPanel.add(mylabel);
-		mainPanel.setBackground( Color.gray );
 		
-		changePanel.setBackground( Color.green );
-		changePanel.add(changeLabel);
+		// add radio buttons	
+			radio1 = new JRadioButton("This option?");
+			radio1.setMnemonic(KeyEvent.VK_B);
+			radio1.setActionCommand("This option?");
+			radio1.setSelected(true);
 		
-		mainPanel.add( changePanel );	
 		
-		this.add( mainPanel );
+			radio2 = new JRadioButton("Maybe This?");
+			radio2.setMnemonic(KeyEvent.VK_C);
+			radio2.setActionCommand("Maybe This?");
+			
+			ButtonGroup group = new ButtonGroup();
+			group.add(radio1);
+			group.add(radio2);
 		
-		radio1 = new JRadioButton("This option?");
-		radio1.setMnemonic(KeyEvent.VK_B);
-		radio1.setActionCommand("This option?");
-		radio1.setSelected(true);
-	
-	
-		radio2 = new JRadioButton("Maybe This?");
-		radio2.setMnemonic(KeyEvent.VK_C);
-		radio2.setActionCommand("Maybe This?");
-	
-		ButtonGroup group = new ButtonGroup();
-		group.add(radio1);
-		group.add(radio2);
-	
-		radio1.addActionListener(this);
-		radio2.addActionListener(this);
-	
+			radio1.addActionListener(this);
+			radio2.addActionListener(this);
+		
 		mainPanel.add(radio1);
 		mainPanel.add(radio2);
+		
+		// add changePanel
+			changePanel = new JPanel();
+			changePanel.setBackground( Color.green );
+			
+			changeLabel = new JLabel("MOTHERFUCKING LABEL");
+			
+			changePanel.add(changeLabel);
+			
+		// add two random panels
+			tempPanel = new JPanel();
+			tempPanel.setBackground( Color.orange );
+			tempPanel.add( new JLabel("MOTHERFUCKING LABEL 1") );
+			
+			panelList.add( tempPanel );
+			
+			
+			tempPanel = new JPanel();
+			tempPanel.setBackground( Color.red );
+			tempPanel.add( new JLabel("MOTHERFUCKING LABEL 2") );
+			
+			panelList.add( tempPanel );
+			
+		// add random panels to main panel
+			Iterator < JPanel >    panelIT =  panelList.iterator();
+	        
+	        while ( panelIT.hasNext() )
+	        {
+	        	JPanel currPanel = panelIT.next();
+	        	
+	        	this.add ( currPanel );
+	        }
+		
+		//mainPanel.add( changePanel );		
+		
+		this.add( mainPanel );
 		
 		}
 		
